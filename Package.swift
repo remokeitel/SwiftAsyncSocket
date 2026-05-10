574df40f52e8c3ca1fb99838e43e84cc21b765ae
Remo Keitel | IT-Schmiede <remo.keitel@itschmie.de>
2026-05-10 10:25:33 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftAsyncSocket"),
+            name: "SwiftAsyncSocket",
+            path: "Source",
+            exclude: ["SupportingFiles/Info.plist"]),
         .testTarget(
             name: "TCP_Test",
             dependencies: ["SwiftAsyncSocket"]),
